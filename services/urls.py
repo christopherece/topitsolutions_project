edf3fe28e4c94ee2c6386aa3db2417650b8d8610
Orewa College <orewacollege@topitsolutions.local>
2021-07-18 22:41:51 +1200

    Added Services

--- services/urls.py
+++ services/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.index, name='services'),
     path('computer_laptop_repair', views.computer_laptop_repair, name='computer_laptop_repair'),
     path('computer_laptop_upgrade', views.computer_laptop_upgrade, name='computer_laptop_upgrade'),
+    path('iot', views.iot, name='iot'),
+    path('networking', views.networking, name='networking'),
+    path('webdevelopment', views.webdevelopment, name='webdevelopment'),
+
 
 ]
--- services/views.py
+++ services/views.py
@@ -9,3 +9,12 @@ def computer_laptop_repair(request):
 
 def computer_laptop_upgrade(request):
     return render(request,'services/computer_laptop_upgrade.html')
+
+def iot(request):
+    return render(request,'services/iot.html')
+
+def networking(request):
+    return render(request,'services/networking.html')
+
+def webdevelopment(request):
+    return render(request,'services/webdevelopment.html')
