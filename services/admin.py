a9e21430f2a35ea4f220e62760cedd7686586a35
Orewa College <orewacollege@topitsolutions.local>
2021-07-29 22:53:38 +1200

    change admit site

--- services/admin.py
+++ services/admin.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from .models import Webprj
 
 class WebprjAdmin(admin.ModelAdmin):
-    list_display = ('id','title','is_published','comment','list_date')
+    list_display = ('id','title','is_published','comment','list_date','body')
 
 admin.site.register(Webprj, WebprjAdmin)
