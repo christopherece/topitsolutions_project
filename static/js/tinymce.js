da2d5075ed7f71b5cc5dbcff5769a23282f14314
Orewa College <orewacollege@topitsolutions.local>
2021-08-01 17:41:16 +1200

    Change Blog Card4

---
+++ static/js/tinymce.js
@@ -0,0 +1,10 @@
+$(function(){
+    tinymce.init({
+        selector: 'link',
+        plugins: 'link',
+        menubar:'insert',
+        toolbar: 'link',
+        default_link_target: "_blank"
+    })
+});
+
